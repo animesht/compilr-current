bd76043bce7e6f0b4e2bfe832b21699e776d72ac
Animesh Tripathi <me@animesh.co>
2014-10-05 04:19:03 -0400

    Testing

---
+++ facebook-20140930-035632_extract.cpp
@@ -0,0 +1,74 @@
+Academ???c Session 2014715
+
+Academic Repon : TERMVI
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+Name Ragvm 5m an Nn 5429 Falhers Name Ralesn 5m
+Clsss xu \ RoH N0 1 Mumrs sze Devma smayee
+smp mux) uma) ???12m Yhanlym) pmmus) 1mm 12m 95
+c1 1mg) 1m
+0m Mu m M-x mx Mn am Mlx m Mlx (Ma
+Mm um Mm m Mlm 0000
+???5???
+Pahu: m ???5 215 5a 27 5n ??5 m9 445 mn mu
+a???
+Suanc
+2mm 9 ???5 355 5a 205 5n 535 mn A a ass uas 6907
+n
+Mzms m ???5 ms 50 ns 5n 255 ??)0 155 715 3325
+Fm:
+M5
+Emna a ??5 135 5a ???55 5n :55 mo n a 355 125 3372
+Psycrx a ???5 :2 35 ??5 35 525 m 25 30 ass uzs 7703
+muwy
+Omra??? 5350 52m
+
+ 
+
+ 
+
+ 
+
+ 
+
---
+++ raw_730694_10152724564315979_1163062000_n_extract.txt
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+int count = 10;
+for (int i = 0; i < count; i++) {
+cout << "for loop : " << i << endl;
+}
+while (count < 10) {
+cout << "while loop : " << count << endl;
+count++;
+}
+return 0;
+}
+
+//End of File
+
